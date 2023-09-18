f722f1413b8a0ce9d3707eb2924f1c688b5b35bd
Hd2572 <hd2003419@sina.com>
2023-09-18 18:02:26 +0800

    9_18

--- TcpClient/main.cpp
+++ TcpClient/main.cpp
@@ -1,12 +1,22 @@
 #include <QApplication>
 
+//#include "friend.h"
+//#include "online.h"
+#include "opewidget.h"
 #include "tcpclient.h"
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    TcpClient w;
+
+    //    Online w;
+    //    w.show();
+    //    TcpClient w;
+    //    w.show();
+    OpeWidget w;
     w.show();
+    //    Friend w;
+    //    w.show();
 
     return a.exec();
 }
